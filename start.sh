12da2e10b4c14948fc1d47b0cfbed90daf396820
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-03-02 04:52:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ POOL=stratum+tcp://na.luckpool.net:3956#xnsub
 WALLET=RR1gZge8garhA1K789W4KUkvUB2uWpvRQi
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 cd "$(dirname "$0")"
-chmod +x 777 * && sudo ./hellminer -u $WALLET.$WORKER -p x --cpu 8 $@
+chmod 777 * && sudo ./hellminer -u $WALLET.$WORKER -p x --cpu 8 $@
