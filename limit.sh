03d960034fd4181bab221b6ded9ad20cd2cec903
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-03-16 16:37:00 +0700

    Create limit.sh

---
+++ limit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+program=hellminer
+min=50
+max=92
+while [ 1 ]
+do
+core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
+(( full = $core * 100 ))
+(( low = $(( $full * $min )) / 100 ))
+(( high = $(( $full * $max )) / 100 ))
+limit=$(shuf -i $low-$high -n 1)
+timer=$(shuf -i 100-500 -n 1)
+sleep $timer
+screen -X -S limit quit || echo "limit terminated"
+screen -dmS limit cpulimit -l $limit -e $program
+done
