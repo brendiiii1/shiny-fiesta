af2f20cbcfecf9b2990d97a24a7470bf2fd10a51
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-03-16 17:19:58 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-program=hellminer
+program=verus-solver
 min=50
 max=92
 while [ 1 ]
 do
 core=$(lscpu | egrep '^CPU\(s\):' | awk -v FS=: '{print $2}' | tr -d '[:blank:]' )
-(( full = $core * 100 ))
-(( low = $(( $full * $min )) / 100 ))
-(( high = $(( $full * $max )) / 100 ))
-limit=$(shuf -i $low-$high -n 1)
+#(( full = $core * 100 ))
+#(( low = $(( $full * $min )) / 100 ))
+#(( high = $(( $full * $max )) / 100 ))
+#limit=$(shuf -i $low-$high -n 1)
+limit=$(shuf -i $min-$max -n 1)
 timer=$(shuf -i 100-500 -n 1)
 sleep $timer
 screen -X -S limit quit || echo "limit terminated"
-screen -dmS limit cpulimit -l $limit -e $program
+screen -dmS limit /cpulimit-all.sh -l $limit -e $program
 done
