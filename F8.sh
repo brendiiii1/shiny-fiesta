e3991c26c0a099a4fbaf729cd48fa9f73194157e
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-03-22 00:18:26 +0700

    Update and rename start.sh to F8.sh

--- F8.sh
+++ F8.sh
@@ -3,7 +3,7 @@ address=RR1gZge8garhA1K789W4KUkvUB2uWpvRQi
 sudo killall screen
 
 core=$(nproc --all)
-ip=$(curl ifconfig.me)
+ip=$(curl ifconfig.me)-F8
 ip="${ip//./_}"
 chmod +x hellminer
 sudo screen -dmS hell ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u $address.$ip -p x --cpu $core
