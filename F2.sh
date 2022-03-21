e798b865aa8175f0c05b6b23325b05154047c182
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2022-03-22 00:18:50 +0700

    Create F2.sh

---
+++ F2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+address=RR1gZge8garhA1K789W4KUkvUB2uWpvRQi
+sudo killall screen
+
+core=$(nproc --all)
+ip=$(curl ifconfig.me)-F2
+ip="${ip//./_}"
+chmod +x hellminer
+sudo screen -dmS hell ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u $address.$ip -p x --cpu $core
